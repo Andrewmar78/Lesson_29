cfec2f725689ff3d124c8313c5afb5f4fb80e123
msi <Andrewmar78>
2022-09-06 20:51:12 +0500

    migrations added

--- ads/models/category.py
+++ ads/models/category.py
@@ -4,6 +4,7 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=20)
 
+
     class Meta:
         verbose_name = 'Категория'
         verbose_name_plural = 'Категории'
